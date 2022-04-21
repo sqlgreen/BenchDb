ddccc9f402add99bada74204dd7f6bbcc1064ed0
Darren Green <darren.green@rabobank.com>
2022-04-21 09:53:28 +0200

    Added split projets and solution for MSBuild.Sdk.SqlProj. Batch files for running a repeated set of tests

---
+++ BuildProject.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "startTime=%time: =0%"
+
+echo Building %1
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" %1 /rebuild Debug
+if ERRORLEVEL 1 echo ** Error %ERRORLEVEL% **
+
+set "endTime=%time: =0%"
+
+
+
+rem Get elapsed time:
+set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+
+rem Convert elapsed time to HH:MM:SS:CC format:
+set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+
+echo Start:    %startTime%
+echo End:      %endTime%
+echo Elapsed:  %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1%
+echo.
---
+++ Runs.bat
@@ -0,0 +1,77 @@
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.sln
+
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDbSplit.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
+
+call BuildProject BenchDbSplit.Build.sln
+call BuildProject BenchDbSplit.sln
+call BuildProject BenchDb.sln
+call BuildProject BenchDb.Build.sln
